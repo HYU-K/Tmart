f818e2f4d91a02ec1d6ca61bcfadd1ad51d758ea
gou_keyy <lehuyhuy353@gmail.com>
2022-10-19 10:35:22 +0700

    huy add

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "decc824a818e23f8"; }
+/******/ 		__webpack_require__.h = function() { return "72a6840a5c12c29f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
